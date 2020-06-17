fa1c6f0dc49356f05de87e4c098339c516eb6a74
Tandjune Tamoyem Florent <tamoyem@rhrk.uni-kl.de>
2020-06-18 01:41:16 +0200

    Fast alle Klassen des <projektes implementiert (Grundgerüst) ohne Dokumentation und Tests

---
+++ Implementierung/ExplodingKittens/src/main/client/SpielenClient.java
@@ -0,0 +1,13 @@
+package main.client;
+
+import main.server.Spielen;
+
+public class SpielenClient implements Spielen {
+
+    public void Main(){}
+
+    @Override
+    public void start() {
+
+    }
+}
---
+++ Implementierung/ExplodingKittens/src/main/server/Spieler.java
@@ -0,0 +1,24 @@
+package main.server;
+
+import main.server.karten.Karte;
+
+import java.util.List;
+
+public class Spieler {
+
+    String nickname;
+    int id;
+    int punkt;
+    List<Karte> handkarte;
+
+    public Spieler() {
+    }
+
+    public void karteLegen(Karte k) {
+    }
+
+    public Karte zugBeenden() {
+
+        return null;
+    }
+}
