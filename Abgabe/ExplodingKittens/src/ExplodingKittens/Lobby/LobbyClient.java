54a5fff1033b8d50da678c4c108adeb889f73ace
Yirui Fu <yiruifu@rhrk.uni-kl.de>
2020-06-20 22:41:04 +0200

    update Exception from Fu

--- Abgabe/ExplodingKittens/src/ExplodingKittens/Lobby/LobbyClient.java
+++ Abgabe/ExplodingKittens/src/ExplodingKittens/Lobby/LobbyClient.java
@@ -2,6 +2,7 @@ package ExplodingKittens.Lobby;
 
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 
 /**
@@ -13,8 +14,9 @@ public interface LobbyClient{
      * Methode die die Lobby darstellt
      *
      * @param stage Stage wird übergeben um die erzeugte Grafik sichtbar zu machen
+     * @throws IOException  Fehler bei IO
      */
-    void displayLobby(Stage stage) throws RemoteException;
+    void displayLobby(Stage stage) throws IOException;
 
     /**
      * Methode um die Bestenliste zu zeigen
