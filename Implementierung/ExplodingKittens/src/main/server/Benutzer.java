1c18273c4bac5241e59a8689a55e0fbb53a67f79
Tandjune Tamoyem Florent <tamoyem@rhrk.uni-kl.de>
2020-06-22 10:20:04 +0200

    Replace Benutzer.java

--- Implementierung/ExplodingKittens/src/main/server/Benutzer.java
+++ Implementierung/ExplodingKittens/src/main/server/Benutzer.java
@@ -2,51 +2,23 @@ package main.server;
 
 public class Benutzer {
 
-    private String email;
-    private String vorname;
-    private String nachname;
-    private String nickname;
+    private String benutzername;
     private String passwort;
+    private int   punkt;
 
-    public Benutzer(String email, String vorname, String nachname, String nickname, String passwort) {
-        this.email = email;
-        this.vorname = vorname;
-        this.nachname = nachname;
-        this.nickname = nickname;
+    public Benutzer( String nickname, String passwort) {
+
+        this.benutzername = nickname;
         this.passwort = passwort;
     }
 
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getVorname() {
-        return vorname;
-    }
-
-    public void setVorname(String vorname) {
-        this.vorname = vorname;
-    }
-
-    public String getNachname() {
-        return nachname;
-    }
-
-    public void setNachname(String nachname) {
-        this.nachname = nachname;
-    }
-
     public String getNickname() {
-        return nickname;
+        return benutzername;
     }
 
     public void setNickname(String nickname) {
-        this.nickname = nickname;
+        this.benutzername = nickname;
     }
 
     public String getPasswort() {
@@ -55,5 +27,13 @@ public class Benutzer {
 
     public void setPasswort(String passwort) {
         this.passwort = passwort;
+    }
+
+    public int getPunkt() {
+        return punkt;
+    }
+
+    public void setPunkt(int punkt) {
+        this.punkt = punkt;
     }
 }
